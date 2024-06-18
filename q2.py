c257d019441f83aac86d1c858620a8cdbc8d9af8
José Gabriel Dâmaso <162503520+texazboy@users.noreply.github.com>
2024-06-18 18:29:44 -0300

    Delete q2.py

--- q2.py
+++
@@ -1,9 +0,0 @@
-valores = []
-
-for i in range(6):
-    valor = int(input("Digite um valor inteiro: "))
-    valores.append(valor)
-
-print("Valores lidos:")
-for valor in valores:
-    print(valor)
